a3675636484b4279b0450ed951639488cc4416b7
fahadali323 <afahad5312@gmail.com>
2023-11-02 22:26:08 -0400

    navigation bar is responsive to different viewports/devices

--- fitness-app/src/components/navbar-visitor/index.js
+++ fitness-app/src/components/navbar-visitor/index.js
@@ -1,16 +1,25 @@
-import React from "react";
-import { Nav, NavLink, NavMenu, Bars } from "./NavbarElements"; // Import Bars
-import { LoginButton, Logo,  LogoImage} from "./NavbarElements";
+import React, { useState } from "react";
+import { Nav, NavLink, NavMenu, Bars, LoginButton, Logo, LogoImage, HamburgerMenu } from "./NavbarElements";
+import { FaBars } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import logo from "../../roles/visitors/assets/logo.png";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
     <>
       <Nav>
-      <Logo>
-          <LogoImage src="./logo.png" alt="Your Logo" />
+        <Logo>
+          <LogoImage src="{logo}" alt="Your Logo" />
         </Logo>
-        <Bars />
-        <NavMenu>
+        <Bars onClick={toggleMenu} />
+        <NavMenu showMenu={showMenu}>
           <NavLink to="/" activeStyle>
             HOME
           </NavLink>
