bbab9b2c1af642552de549936f46019106e03d7c
fahadali323 <afahad5312@gmail.com>
2023-11-18 21:32:30 -0500

    added filtering for state,town but name and state&town has issue

--- fitness-app/src/roles/client/pages/clientcoaches.js
+++ fitness-app/src/roles/client/pages/clientcoaches.js
@@ -7,14 +7,48 @@ function ClientCoaches() {
   const [currentPage, setCurrentPage] = useState(1);
   const [coachesPerPage] = useState(5);
   const [searchTerm, setSearchTerm] = useState("");
+  const [filters, setFilters] = useState({
+    type: "",
+    value: "",
+  });
 
   useEffect(() => {
     fetchData();
-  }, []);
+  }, [filters, searchTerm]); // Trigger fetch on filter and search term changes
 
   const fetchData = async () => {
     try {
-      const response = await fetch("http://127.0.0.1:5000/coaches");
+      let url = "http://127.0.0.1:5000/coaches";
+
+      // Add filters to the URL if values are selected
+      if (filters.type && filters.value) {
+        switch (filters.type) {
+          case "name":
+            url = `http://127.0.0.1:5000/coaches/filter/name/${filters.value}`;
+            break;
+          case "gym":
+            url = `http://127.0.0.1:5000/coaches/filter/gym/${filters.value}`;
+            break;
+          case "state":
+            url = `http://127.0.0.1:5000/coaches/filter/state/${filters.value}`;
+            break;
+          case "town":
+            url = `http://127.0.0.1:5000/coaches/filter/town/${filters.value}`;
+            break;
+          case "stateTown":
+            url = `http://127.0.0.1:5000/coaches/filter/statetown/${filters.value}`;
+            break;
+          default:
+            break;
+        }
+      }
+
+      // Add search term to the URL if it is present
+      if (searchTerm) {
+        url += `/search/${searchTerm}`;
+      }
+
+      const response = await fetch(url);
       const data = await response.json();
       setCoaches(data);
     } catch (error) {
@@ -34,59 +68,86 @@ function ClientCoaches() {
 
   // Search
   const handleSearch = () => {
-    const filteredCoaches = coaches.filter(
-      (coach) =>
-        coach.firstname.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        coach.lastname.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setCoaches(filteredCoaches);
-    setCurrentPage(1); // Reset to the first page after filtering
+    fetchData();
+  };
+
+  // Filter functions
+  const handleFilterChange = (selectedFilter) => {
+    setFilters({
+      type: selectedFilter,
+      value: "",
+    });
+  };
+
+  const handleFilter = () => {
+    fetchData();
+  };
+
+  const handleClear = () => {
+    setSearchTerm("");
+    setFilters({
+      type: "",
+      value: "",
+    });
   };
 
   return (
     <div className="body_1">
       <ClientNavbar />
       <h1>COACHES</h1>
-
       <div className="search-container">
         <input
           type="text"
-          placeholder="Search by name"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search"
+          value={filters.value}
+          onChange={(e) => setFilters({ ...filters, value: e.target.value })}
         />
-        <button onClick={handleSearch}>Search</button>
+        <select
+          value={filters.type}
+          onChange={(e) => handleFilterChange(e.target.value)}
+        >
+          <option value="" disabled>
+            Filter by:
+          </option>
+          <option value="name">Name</option>
+          <option value="gym">Gym</option>
+          <option value="state">State</option>
+          <option value="town">Town</option>
+          <option value="stateTown">State & Town</option>
+        </select>
+        <button onClick={handleFilter}>Filter</button>
+        <button onClick={handleClear}>Clear</button>
       </div>
 
       {currentCoaches.map((coach) => (
         <tr key={coach.clientID}>
           <div className="profile">
             <div className="left">
-              <name>
-                {coach.firstname}
-              </name>
-              <name2>
-              {coach.lastname}
-              </name2> 
-              <age>AGE: </age>
-              <price>PRICE: ${coach.price}</price>
+              <name>{coach.firstname}</name>
+              <name2>{coach.lastname}</name2>
+              <age>Age: </age>
+              <price>Price: ${coach.price}</price>
+              <gym>Gym: {coach.gym}</gym>
             </div>
 
             <div className="middle">
-              <h3>DESCRIPTION</h3>
-              {coach.bio}
+              <div className="location">
+                <location>LOCATION</location>
+                <town>Town: {coach.town} </town>
+                <state>State: {coach.state}</state>
+              </div>
             </div>
 
             <div className="right">
-              <h3>CONTACT</h3>
-              {coach.email} {coach.phone}
+              <div className="contact">
+                <contact>CONTACT</contact>
+                <email>{coach.email}</email>
+              </div>
               <button className="view">VIEW PROFILE</button>
             </div>
           </div>
         </tr>
       ))}
-
-      {/* Pagination */}
       <div className="pagination">
         <button
           onClick={() => paginate(currentPage - 1)}
