3ddb328a1eafe4e17d3eb47062e5e8a07b3718de
fahadali323 <afahad5312@gmail.com>
2023-11-18 21:40:46 -0500

    added radio to registration for registering as coach or client

--- fitness-app/src/roles/visitors/registration.js
+++ fitness-app/src/roles/visitors/registration.js
@@ -1,12 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import './registration.css'
+import "./registration.css";
 import VistorNavbar from "../../components/navbar-visitor/visitornav.js";
 
-
-
-const API_URL = 'http://127.0.0.1:5000';
+const API_URL = "http://127.0.0.1:5000";
 
 const Registration = () => {
   const [formData, setFormData] = useState({
@@ -28,17 +26,22 @@ const Registration = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    
-    if (!formData.firstname || !formData.lastname || !formData.email || !formData.password) {
+
+    if (
+      !formData.firstname ||
+      !formData.lastname ||
+      !formData.email ||
+      !formData.password
+    ) {
       console.error("All fields are required.");
       return;
     }
 
     try {
       const response = await fetch(`${API_URL}/signup`, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify(formData),
       });
@@ -56,61 +59,76 @@ const Registration = () => {
     } catch (error) {
       console.error("Error", error.message);
     }
-  }
+  };
 
   return (
     <div className="registration-page">
-       <VistorNavbar />
-       <div className="login-container">
-      <div className="registration-modal">
-        <h1>Registration</h1>
-        <form onSubmit={handleSubmit} className="registration-form">
-          <div>
-            <label>First Name </label>
-            <input
-              type="text"
-              name="firstname"
-              value={formData.firstname}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Last Name </label>
-            <input
-              type="text"
-              name="lastname"
-              value={formData.lastname}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Email </label>
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div>
-            <label>Password </label>
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <button type="submit">Sign Up</button>
-        </form>
-        <p>
-          Already have an account? <Link className="link" to="/login">Login here</Link>
-        </p>
-      </div>
+      <VistorNavbar />
+      <div className="login-container">
+        <div className="registration-modal">
+          <h1>Registration</h1>
+          <form onSubmit={handleSubmit} className="registration-form">
+            <div>
+              <label>First Name </label>
+              <input
+                type="text"
+                name="firstname"
+                value={formData.firstname}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Last Name </label>
+              <input
+                type="text"
+                name="lastname"
+                value={formData.lastname}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Email </label>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div>
+              <label>Password </label>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+            <div className="roles-container">
+              <label>Roles:</label>
+              <label>
+                <input type="radio" name="roles" value="1" required />
+                Coach
+              </label>
+              <label>
+                <input type="radio" name="roles" value="0" required />
+                Client
+              </label>
+            </div>
+
+            <button type="submit">Sign Up</button>
+          </form>
+          <p>
+            Already have an account?{" "}
+            <Link className="link" to="/login">
+              Login here
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   );
