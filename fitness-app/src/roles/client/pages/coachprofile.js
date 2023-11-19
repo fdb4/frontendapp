37158be8def92001865dd1e506f085c7e0e8caa8
fahadali323 <afahad5312@gmail.com>
2023-11-19 18:17:32 -0500

    fixed api endpoint on coachprofile.js

--- fitness-app/src/roles/client/pages/coachprofile.js
+++ fitness-app/src/roles/client/pages/coachprofile.js
@@ -10,7 +10,7 @@ const CoachProfile = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://127.0.0.1:5000/coaches`);
+        const response = await fetch(`http://127.0.0.1:5000/coaches/${id}`);
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
