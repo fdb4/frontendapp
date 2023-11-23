1492dae5d899405e326785b34123ed392bcd1181
Sandeep Singh <singh.deep8302@gmail.com>
2023-11-23 08:05:06 -0500

    still needs fixing

--- fitness-app/src/roles/visitors/pages/coachsurvey.js
+++ fitness-app/src/roles/visitors/pages/coachsurvey.js
@@ -2,18 +2,25 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import axios from 'axios';
-// import "./initialsurveypage.css";
-import VistorNavbar from "../../components/navbar-visitor/visitornav.js";
+import "../styling/initialsurveypage.css";
 import CurrencyInput from 'react-currency-input-field'
 import TextField from '@mui/material/TextField';
-import AdapterDateFns from '@mui/material/AdapterDateFns';
-import LocalizationProvider from '@mui/material/LocalizationProvider';
-import DatePicker from '@mui/material/DatePicker';
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
+import InputAdornment from '@mui/material/InputAdornment';
 
 const API_URL = "http://127.0.0.1:5000";
 
 const CoachSurvey = () => {
+    const [date, setDate] = useState(null);
+    const [price, setPrice] = useState(null);
+    const [priceError, setPriceError] = useState("");
+    
+    const handleDateChange = (newValue) => {
+        setDate(newValue);
+    }; 
+
     const [data, setData] = useState({
         id: '',
         price: '',
@@ -26,17 +33,28 @@ const CoachSurvey = () => {
 
     const handleChange = (e) => {
     	const { name, value } = e.target;
-    	setFormData({
-    	  ...formData,
+    	setData({
+    	  ...data,
     	  [name]: value,
     	});
  	};
 
      const handleSubmit = async (e) => {
     	e.preventDefault();
+        const originalDate = new Date(date);
+        const formattedDate = originalDate.toLocaleDateString('en-US', { year: 'numeric', month: '2-digit', day: '2-digit' });
+        console.log(formattedDate)
+        setData({
+            ...data,
+            ['experience']: 'hey'
+        })
+        setData({
+            ...data,
+            ['price']: price
+        })
 
     	if (
-	      !data.id ||
+	    //   !data.id ||
     	  !data.price ||
 	      !data.experience ||
     	  !data.bio ||
@@ -44,12 +62,21 @@ const CoachSurvey = () => {
     	  !data.town ||
 	      !data.state) 
     	{
+            console.log(data)
      	 console.error("All fields are required.");
      	 return;
     	}
+
+        const priceValue = parseFloat(price);
+        if (isNaN(priceValue) || priceValue < 0) {
+          setPriceError("Please enter a valid positive float for price");
+          return;
+        } else {
+          setPriceError(null);
+        }
         console.log(data)
     	try {
-      		const comm = await axios.post(`${API_URL}/signup`, data);
+      		const comm = await axios.post(`${API_URL}/coachSignUp`, data);
       		console.log("Registering with data:", data);
       		console.log("Response:", comm.data);
     	}
@@ -78,31 +105,41 @@ const CoachSurvey = () => {
                         <h2>Extra Information</h2>
                         <div>
                             <label>Hourly Price: </label>
-                            <CurrencyInput
-                                name="price"
-                                value={data.price}
-                                onValueChange={(value) => handleChange('price', value)}
-                                prefix="$"
-                                decimalsLimit={2} 
-                                required
-                            />                       
+                            <TextField
+                                id="price"
+                                variant="filled"
+                                error={Boolean(priceError)}
+                                helperText={priceError || ' '}
+                                required type = "number"
+                                value={price}
+                                onChange={(event) => setPrice(event.target.value)}
+                                sx={{ width: '300px'}}
+                            />                  
                         </div>
+
                         <LocalizationProvider dateAdapter={AdapterDateFns}>
                         <div>
                             <label>First Coaching Experience Date:</label>
                             <DatePicker
-                            name="experience"
-                            value={data.experience}
-                            onChange={handleChange}
-                            renderInput={(props) => <TextField {...props} />}
+                            renderInput={(props) => (
+                                <TextField
+                                {...props}
+                                InputProps={{ style: { color: 'black' } }} // Set the desired text color
+                                />
+                            )}
+                            value={date}
+                            onChange={handleDateChange}
+                            format="yyyy-MM-dd"
                             required
                             />
                         </div>
                         </LocalizationProvider>
+
+                        <br />
+
                         <div>
-                        <lable>Gym Name</lable>
+                        <label>Gym Name</label>
                         <TextField
-                            label="Enter Text"
                             variant="outlined"
                             fullWidth
                             name="gym"
@@ -111,10 +148,12 @@ const CoachSurvey = () => {
                             required
                         />
                         </div>
+
+                        <br />
+
                         <div>
-                        <lable>Current Town</lable>
+                        <label>Current Town</label>
                         <TextField
-                            label="Enter Text"
                             variant="outlined"
                             fullWidth
                             name="town"
@@ -123,10 +162,12 @@ const CoachSurvey = () => {
                             required
                         />
                         </div>
+                        
+                        <br />
+
                         <div>
-                        <lable>Current State</lable>
+                        <label>Current State</label>
                         <TextField
-                            label="Enter Text"
                             variant="outlined"
                             fullWidth
                             name="state"
@@ -135,7 +176,11 @@ const CoachSurvey = () => {
                             required
                         />
                         </div>
+
+                        <br />
+
                         <div>
+                        <label>Bio</label>
                         <TextareaAutosize
                             aria-label="bio-input"
                             minRows={3} // Set the minimum number of rows
@@ -143,9 +188,10 @@ const CoachSurvey = () => {
                             name="bio"
                             value={data.bio}
                             onChange={handleChange}
-                            style={{ width: '100%' }}
+                            style={{ width: '95%' }}
                         />
                         </div>
+                        
                         <button type="submit" className="submit-button">Submit Survey</button>
                     </form> 
                 </div>
@@ -153,3 +199,5 @@ const CoachSurvey = () => {
         </div>
     );
 }
+
+export default CoachSurvey
