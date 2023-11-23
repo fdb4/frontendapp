2ebc34b59646e110e8764c1f78c0ef682cab554e
Sandeep Singh <singh.deep8302@gmail.com>
2023-11-23 02:11:11 -0500

    got name on homepage

--- fitness-app/src/roles/client/pages/clienthome.js
+++ fitness-app/src/roles/client/pages/clienthome.js
@@ -4,18 +4,32 @@ import { Link } from 'react-router-dom';
 import facebook from "../../visitors/assets/facebook.png"
 import instagram from "../../visitors/assets/instagram.png"
 import linkedin from "../../visitors/assets/linkedin.png"
+import Cookies from 'js-cookie';
+import axios from 'axios';
 
 function ClientHome() {
     const [firstName, setFirstName] = useState('')
-    //Need to Import the UsersName/ID
+    const [lastName, setLastName] = useState('')
+    const id = Cookies.get('id')
 
+    useEffect(() => {
+        axios.get(`http://127.0.0.1:5000/clients/${id}`)
+        .then((response) => {
+            setFirstName(response.data[0].firstname)
+            setLastName(response.data[0].lastname)
+        })
+        .catch((error) => 
+            {console.log(error.message)});
+        },[]); 
+    //Need to Import the UsersName/ID
+    
     return(
         <div className="body_1">
             <ClientNavbar />
             <h1>Home Page</h1>
             <p9>Welcome to BitFit, Health for everyone!</p9>
-            <h3>First Name</h3>
-            <h3>Last Name</h3>
+            <h3>{firstName}</h3>
+            <h3>{lastName}</h3>
             <div>
                 <Link to="/workouts">
                 <button>Create Workout</button>
