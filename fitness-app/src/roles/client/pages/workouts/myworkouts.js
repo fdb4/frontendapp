9c8bcda469dbdc76bb7edc1d52af957c1e33b53f
fahadali323 <afahad5312@gmail.com>
2023-11-29 13:00:16 -0500

    working on myworkouts page

--- fitness-app/src/roles/client/pages/workouts/myworkouts.js
+++ fitness-app/src/roles/client/pages/workouts/myworkouts.js
@@ -1,48 +1,36 @@
 import React from "react";
 import ClientNavbar from "../../../../components/navbar-visitor/clientnav";
-import { useNavigate } from "react-router-dom"; 
+import { useNavigate } from "react-router-dom";
+import "../../styling/myworkouts.css";
+import Pullups from "../../../visitors/assets/pullup.jpg";
 
 function Myworkouts() {
-    const navigate = useNavigate();
-    const handleGoBack = () => {
-        // Go back to the previous page in the history
-        navigate(-1);
-      };
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate("/workouts");
+  };
 
   return (
     <div className="body">
-      <ClientNavbar/>
+      <ClientNavbar />
       <div className="individual">
         <div className="rightside">
           <h1>My Workout</h1>
           <button onClick={handleGoBack}>Back</button>
           <div>
-           <figure>
-                <img></img>
-                <figcatption>
-                    <h4>PULL WORKOUT</h4>
-                    <h5>45-60min</h5>
-                </figcatption>
-                <span>
-                    <button>
-                        <span>
-                            button
-                        </span>
-                    </button>
-                </span>
-           </figure>
-            
+            <figure>
+              <img src={Pullups} alt="pullups image" />
+              <figcaption>
+                <h4 className="titles">PULL WORKOUT</h4>
+                <h5>45-60min</h5>
+              </figcaption>
+            </figure>
           </div>
           <h4>Plan Info</h4>
-          <div>
-
-          </div>
+          <div></div>
         </div>
-        <div className="leftside">
-
-        </div>
+        <div className="leftside"></div>
       </div>
-
 
       {/* <div clasName="coach_recommendation">
 
