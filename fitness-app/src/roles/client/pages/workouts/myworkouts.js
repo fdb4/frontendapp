bfe326b75c06678866c7dcfd3071f2ccccd56813
fahadali323 <afahad5312@gmail.com>
2023-11-29 15:03:45 -0500

    finished left side of myworkouts

--- fitness-app/src/roles/client/pages/workouts/myworkouts.js
+++ fitness-app/src/roles/client/pages/workouts/myworkouts.js
@@ -15,8 +15,10 @@ function Myworkouts() {
       <ClientNavbar />
       <div className="individual">
         <div className="rightside">
-          <h1>My Workout</h1>
-          <button onClick={handleGoBack}>Back</button>
+          <div className="header">
+            <h1>My Workout</h1>
+            <button onClick={handleGoBack}>Back</button>
+          </div>
           <div>
             <figure>
               <img src={Pullups} alt="pullups image" />
@@ -27,11 +29,24 @@ function Myworkouts() {
             </figure>
           </div>
           <h4>Plan Info</h4>
-          <div></div>
+          <div className="workoutinfo">
+            <h5 className="title">Push-Pull-Legs</h5>
+            <div className="par">
+              <div className="par-item">6 days per week</div>
+              <div className="par-item">66-70 mins per day</div>
+            </div>
+            <hr /> 
+            <p>
+              A beginner routine with six days a week of lifting and a focus on
+              barbell lifts. Two lower body workouts, two pul workouts, and two
+              push workouts per day. The routine is very popular.
+            </p>
+            <button className="edit">Edit</button>
+          </div>    
         </div>
         <div className="leftside"></div>
-      </div>
 
+      </div>
       {/* <div clasName="coach_recommendation">
 
       </div> */}
