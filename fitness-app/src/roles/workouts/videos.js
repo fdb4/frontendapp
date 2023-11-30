0e3aa6e58b150e2d67be2ea6dca2ebd4bcefe60d
fahadali323 <afahad5312@gmail.com>
2023-11-30 14:34:50 -0500

    added more information to exercises

--- fitness-app/src/roles/workouts/videos.js
+++ fitness-app/src/roles/workouts/videos.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./videos.css";
 
 function Videos({ searchQuery }) {
-  const videosPerPage = 9;
+  const videosPerPage = 9; 
   const [currentPage, setCurrentPage] = useState(1);
   const [goToPage, setGoToPage] = useState("");
 
@@ -10,8 +10,7 @@ function Videos({ searchQuery }) {
     {
       title: "Push Ups",
       url: "https://www.youtube.com/embed/IODxDxX7oi4",
-      description:
-        `Push-ups are a bodyweight exercise targeting the chest, shoulders, and triceps. 
+      description: `Push-ups are a bodyweight exercise targeting the chest, shoulders, and triceps. 
         Start in a plank position, lower your body towards the ground by bending your elbows, and then push back up`,
       muscleGroup: "chest",
       equipment: "None",
@@ -19,73 +18,102 @@ function Videos({ searchQuery }) {
     {
       title: "Incline Bench Press",
       url: "https://www.youtube.com/embed/ajdFwa-qM98",
-      description: 
-      `This is a chest exercise performed on an inclined bench. It targets
+      description: `This is a chest exercise performed on an inclined bench. It targets
       the upper chest muscles. User a barbell or dumbbells and press them upwards
       while laying on the incline bench`,
-      muscleGroup:"chest",
-      equipment: "inlcine bench, barbell or dumbbells"
+      muscleGroup: "chest",
+      equipment: "inlcine bench, barbell or dumbbells",
     },
     {
       title: "Bench Press",
       url: "https://www.youtube.com/embed/4T9UQ4FBVXI?si=NiViPRBdxEqE0-kb",
-      description:
-      `A classic chest exercise involving pressing a barbell away from the chest.
+      description: `A classic chest exercise involving pressing a barbell away from the chest.
        It Works the chest, shouders, and triceps.`,
-       muscleGroup: "upper-body",
-       equipment:"flat bench, barbell"
+      muscleGroup: "chest",
+      equipment: "flat bench, barbell",
     },
     {
       title: "Cable Cross",
       url: "https://www.youtube.com/embed/taI4XduLpTk?si=KopWEkjJLa1BwdpN",
       description: `Cable crowssovers target the chest muscles by pulling cables 
         from opposite sides, crowssing them in front of the body.`,
-        muscleGroup: "upper-body",
-        equipment: "cable machine"
+      muscleGroup: "chest",
+      equipment: "cable machine",
     },
     {
       title: "Dumbbell Flyes",
       url: "https://www.youtube.com/embed/QENKPHhQVi4?si=J0gFTFYeTbkl0LMx",
+      description: `Dumbbell flyes are an isolation exercise that targets the chest muscles. Lie on a flat bench with a dumbbell in each hand, arms extended upward. Lower the weights in a wide arc, feeling a stretch in your chest, then bring them back up to the starting position.`,
+      muscleGroup: "chest",
+      equipment: "dumbells",
     },
     {
       title: "DeadLift",
       url: "https://www.youtube.com/embed/4AObAU-EcYE?si=rr8aOJe80QkzlgDh",
+      description: `The deadlift is a compound movement that engages multiple muscle groups, including the hamstrings, glutes, lower back, and core. Stand in front of a loaded barbell, bend at the hips and knees, grasp the bar, and lift it by straightening your hips and knees.`,
+      muscleGroup: "back",
+      equipment: "barbell"
     },
     {
       title: "Bent Over Rows",
       url: "https://www.youtube.com/embed/FWJR5Ve8bnQ?si=0aDw47IpWIrWSG77",
+      description: `Bent over rows target the muscles in the upper and middle back. Bend at the hips, keep your back straight, and pull the weight (barbell or dumbbells) towards your lower chest while keeping your elbows close to your body.`,
+      muscleGroup: "back",
+      equipment: "barbell, dumbells"
     },
     {
       title: "Lat Pulldowns",
       url: "https://www.youtube.com/embed/SALxEARiMkw?si=4dk7up3pYU7XArcs",
+      description: `Lat pulldowns focus on the latissimus dorsi muscles. Sit at a cable machine, grasp the bar with a wide grip, and pull it down towards your chest, engaging your back muscles.`,
+      muscleGroup: "back",
+      equipment: "cable machine"
     },
     {
       title: "Pull-Ups",
       url: "https://www.youtube.com/embed/sIvJTfGxdFo?si=_B9JcL3WrQIeo4TQ",
+      description:`Pull-ups are a bodyweight exercise targeting the upper back and arms. Hang from a bar with an overhand grip, then pull your body upward until your chin is above the bar.`,
+      muscleGroup: "back",
+      equipment: "pull-up bar",
     },
     {
       title: "T-Bar Rows",
       url: "https://www.youtube.com/embed/m2KAkQV7pdA?si=uT8yImuu_qNIAvLI",
+      description: `T-Bar rows involve using a T-Bar row machine or attaching a barbell to a landmine. Bend at the hips, grip the handle, and row the weight towards your chest.`,
+      muscleGroup: "back",
+      equipment: "t-bar row machine"
     },
     {
       title: "Military Press",
       url: "https://www.youtube.com/embed/QAQ64hK4Xxs?si=3Xdi5TXI7Afm33qA",
+      description: `The military press targets the deltoid muscles. While standing or sitting, press the weight (barbell or dumbbells) overhead from shoulder height.`,
+      muscleGroup: "shoulder",
+      equipment: "barbell, dumbells"
     },
     {
       title: "Lateral Raises",
       url: "https://www.youtube.com/embed/q9LhHrHShs4?si=abfuCDgQTao66OmP",
-    },
+      description: `Lateral raises isolate the side deltoids. Hold a dumbbell in each hand by your sides and lift them out to the sides until your arms are parallel to the ground.`,
+      muscleGroup: "shoulder",
+      equipment: "dumbells"
+   },
     {
       title: "Front Raises",
       url: "https://www.youtube.com/embed/DiNYcgchguo?si=I6j9e_TPOgrY351C",
+      description: `Front raises target the front deltoids. Hold a dumbbell in each hand, arms straight, and lift the weights forward until they reach shoulder height.`,
+      muscleGroup:" shoulder",
+      equipment: "dumbbells"
     },
     {
       title: "Upright Rows",
       url: "https://www.youtube.com/embed/Fq67opsS_hc?si=EOyz7aIYwuepIxp5",
+      description: ` Upright rows work the upper traps and deltoids. Lift the weight (barbell or dumbbells) vertically towards your chin, keeping it close to your body.`,
+      muscleGroup: "shoulders",
+      equipment: "barbell, dumbbells"
     },
     {
       title: "Shrugs",
       url: "https://www.youtube.com/embed/C6sYjDFuq9I?si=I4LuQRCn7Wbv15mL",
+      
     },
     {
       title: "Bicep Curls",
@@ -220,7 +248,7 @@ function Videos({ searchQuery }) {
 
   const handlePageChange = (newPage) => {
     setCurrentPage(newPage);
-    setGoToPage("");
+    setGoToPage(""); 
   };
 
   const handlePreviousPage = () => {
@@ -259,8 +287,7 @@ function Videos({ searchQuery }) {
       <div className="pagination">
         <button onClick={handlePreviousPage}>&lt; Previous</button>
         <span>
-          Page {currentPage} of {totalPages} | Total Videos:{" "}
-          {filteredVideos.length}
+          Page {currentPage} of {totalPages} | Total Videos: {filteredVideos.length}
         </span>
         <span>
           Go to Page:
@@ -272,6 +299,7 @@ function Videos({ searchQuery }) {
           <button onClick={handleGoToPage}>Go</button>
         </span>
         <button onClick={handleNextPage}>Next &gt;</button>
+
       </div>
     </div>
   );
