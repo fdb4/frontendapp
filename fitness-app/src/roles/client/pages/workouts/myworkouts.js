5e23d7d6aa70fed895edaf251daff0516aa9643f
fahadali323 <afahad5312@gmail.com>
2023-12-02 19:22:38 -0500

    added create workout to myworkout.js'

--- fitness-app/src/roles/client/pages/workouts/myworkouts.js
+++ fitness-app/src/roles/client/pages/workouts/myworkouts.js
@@ -1,103 +1,200 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ClientNavbar from "../../../../components/navbar-visitor/clientnav";
 import { useNavigate } from "react-router-dom";
 import "./styling/myworkouts.css";
-import Pullups from "../../../visitors/assets/pullup.jpg";
 
 function Myworkouts() {
   const navigate = useNavigate();
-  const [isEditing, setIsEditing] = useState(false);
-  const [selectedWorkout, setSelectedWorkout] = useState("Push-Pull-Legs");
-  const [workoutsPerWeek, setWorkoutsPerWeek] = useState(6);
-  const [sessionDuration, setSessionDuration] = useState(60);
+  const [limitReachedMessage, setLimitReachedMessage] = useState("");
+  const [error, setError] = useState("");
+
+  const [showWorkoutForm, setShowWorkoutForm] = useState(false);
+  const [workoutPlan, setWorkoutPlan] = useState({
+    sessionsName: "",
+    selectedExercises: [],
+  });
+  const [allExercises, setAllExercises] = useState([]);
+
+  useEffect(() => {
+    const fetchExercises = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/workouts");
+        const exercises = await response.json();
+        setAllExercises(exercises);
+      } catch (error) {
+        setError("Error fetching exercises. Please try again.");
+      }
+    };
+
+    fetchExercises();
+  }, []);
 
   const handleGoBack = () => {
     navigate("/workouts");
   };
 
-  const handleEdit = () => {
-    setIsEditing(true);
+  const handleCreateWorkout = () => {
+    setShowWorkoutForm(true);
   };
 
-  const handleWorkoutSelect = (workoutType) => {
-    setSelectedWorkout(workoutType);
-    setIsEditing(false);
+  const handleCancelCreateWorkout = () => {
+    setShowWorkoutForm(false);
   };
 
-  const handleSaveChanges = () => {
-    // Implement logic to save changes to backend or local state
-    setIsEditing(false);
+
+  const handleAddExercise = () => {
+    // Check if the limit (10 exercises) has been reached
+    if (workoutPlan.selectedExercises.length < 10) {
+      setWorkoutPlan((prev) => ({
+        ...prev,
+        selectedExercises: [
+          ...prev.selectedExercises,
+          { exerciseID: null, sets: 0, reps: 0, time: 0 },
+        ],
+      }));
+    } else {
+      setLimitReachedMessage("You've reached the limit of 10 exercises per session.");
+    }
   };
 
+  const handleDeleteExercise = (index) => {
+    setLimitReachedMessage(""); // Clear the limitReachedMessage
+    setWorkoutPlan((prev) => {
+      const updatedExercises = [...prev.selectedExercises];
+      updatedExercises.splice(index, 1);
+      return { ...prev, selectedExercises: updatedExercises };
+    });
+  };
+
+  const handleInputChange = (index, event) => {
+    const { name, value } = event.target;
+    setWorkoutPlan((prev) => {
+      const updatedExercises = [...prev.selectedExercises];
+      updatedExercises[index][name] = value;
+      return { ...prev, selectedExercises: updatedExercises };
+    });
+  };
+
+  const handleSessionNameChange = (event) => {
+    const { value } = event.target;
+    setWorkoutPlan((prev) => ({ ...prev, sessionsName: value }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+  
+    try {
+      const response = await fetch("http://127.0.0.1:5000/workouts", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(workoutPlan),
+      });
+  
+      if (response.ok) {
+        // Workout plan submitted successfully
+        console.log("Workout Plan submitted:", workoutPlan);
+        // Optionally, reset the form or navigate away
+        setShowWorkoutForm(false);
+      } else {
+        // Handle server error
+        console.error("Failed to submit workout plan:", response.statusText);
+      }
+    } catch (error) {
+      // Handle network error
+      console.error("Network error:", error.message);
+    }
+  };
+  
   return (
     <div className="body">
       <ClientNavbar />
       <div className="individual">
         <div className="rightside">
           <div className="header">
-            <h1>My Workout</h1>
             <button onClick={handleGoBack}>Back</button>
+            <h1>My Workout</h1>
+            {showWorkoutForm ? (
+              <>
+                <button onClick={handleCreateWorkout}>Create Workout</button>
+              </>
+            ) : (
+              <button onClick={handleCreateWorkout}>Create Workout</button>
+            )}
           </div>
-          <div>
-            <figure>
-              <img src={Pullups} alt="pullups image" />
-              <figcaption>
-                <h4 className="titles">{selectedWorkout}</h4>
-                <h5>{`${sessionDuration}-${sessionDuration + 15}min`}</h5>
-              </figcaption>
-            </figure>
-          </div>
-          <h4>Plan Info</h4>
-          <div className="workoutinfo">
-            <h5 className="title">{selectedWorkout}</h5>
-            <div className="par">
-              <div className="par-item">{`${workoutsPerWeek} days per week`}</div>
-              <div className="par-item">{`${sessionDuration} mins per day`}</div>
-            </div>
-            <hr />
-            <p className="workout_description">
-              A beginner routine with {workoutsPerWeek} days a week of lifting and a focus on
-              barbell lifts. Lift hard and most importantly rest well too!
-            </p>
-            <button className="edit" onClick={handleEdit}>
-              Edit
-            </button>
+          <div className="formstyling">
+            {showWorkoutForm && (
+              <form onSubmit={handleSubmit}>
+                <label>Sessions Name:</label>
+                <input
+                  type="text"
+                  name="sessionsName"
+                  value={workoutPlan.sessionsName}
+                  onChange={handleSessionNameChange}
+                />
+                {workoutPlan.selectedExercises.map((exercise, index) => (
+                  <div key={index} className="exercise-form">
+                    <label>Select Exercise:</label>
+                    <select
+                      name="exerciseID"
+                      value={exercise.exerciseID || ""}
+                      onChange={(event) => handleInputChange(index, event)}
+                    >
+                      <option value="" disabled>
+                        Select an Exercise
+                      </option>
+                      {allExercises.map((option) => (
+                        <option key={option.workoutID} value={option.workoutID}>
+                          {option.workoutname}
+                        </option>
+                      ))}
+                    </select>
+                    <label>Sets:</label>
+                    <input
+                      type="number"
+                      name="sets"
+                      value={exercise.sets}
+                      onChange={(event) => handleInputChange(index, event)}
+                    />
+                    <label>Reps:</label>
+                    <input
+                      type="number"
+                      name="reps"
+                      value={exercise.reps}
+                      onChange={(event) => handleInputChange(index, event)}
+                    />
+                    <label>Time:</label>
+                    <input
+                      type="number"
+                      name="time"
+                      value={exercise.time}
+                      onChange={(event) => handleInputChange(index, event)}
+                    />
+                    <button
+                      type="button"
+                      onClick={() => handleDeleteExercise(index)}
+                    >
+                      Delete Exercise
+                    </button>
+                  </div>
+                ))}
+                <button type="button" onClick={handleAddExercise}>
+                  Add Exercise
+                </button>
+                {limitReachedMessage && <p>{limitReachedMessage}</p>}
+                {error && <p>{error}</p>}
+                <div>
+                  <button type="submit">Submit Workout Plan</button>
+                  <button className="cancel_2" type="button" onClick={handleCancelCreateWorkout}>
+                    Cancel
+                  </button>
+                </div>
+              </form>
+            )}
           </div>
         </div>
-        <div className="leftside"></div>
       </div>
-
-      {isEditing && (
-        <div className="workout-modal">
-          <h2>Edit Workout Details</h2>
-          <h2>Select Workout Type</h2>
-          <button onClick={() => handleWorkoutSelect("Push-Pull-Legs")}>
-            Push-Pull-Legs
-          </button>
-          <button onClick={() => handleWorkoutSelect("Other Workout Type")}>
-            Other Workout Type
-          </button>
-          <label>
-            Workouts Per Week:
-            <input
-              type="number"
-              value={workoutsPerWeek}
-              onChange={(e) => setWorkoutsPerWeek(parseInt(e.target.value))}
-            />
-          </label>
-          <label>
-            Session Duration (minutes):
-            <input
-              type="number"
-              value={sessionDuration}
-              onChange={(e) => setSessionDuration(parseInt(e.target.value))}
-            />
-          </label>
-          {/* You can include workout type selection here if needed */}
-          <button onClick={handleSaveChanges}>Save Changes</button>
-          <button onClick={() => setIsEditing(false)}>Cancel</button>
-        </div>
-      )}
     </div>
   );
 }
