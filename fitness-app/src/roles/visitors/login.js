7c88c4488a7a7ddb4e4a208cd21910dbdb7a8c5f
Sandeep Singh <singh.deep8302@gmail.com>
2023-12-06 19:15:16 -0500

    login change

--- fitness-app/src/roles/visitors/login.js
+++ fitness-app/src/roles/visitors/login.js
@@ -64,7 +64,7 @@ const Login = () => {
         setLoginMessage(data.message);
         const id = data?.clientID;
         let role = "";
-        if (data?.isCoach === 0) {
+        if (data?.coachexpID === 0) {
           role = "Client";
         } else {
           role = "Coach";
